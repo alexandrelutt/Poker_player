d9dbbb1809a3562d8a0fe8c1c5ef80a343214c81
Alexandre Lutt <alexandre.lutt@eleves.enpc.fr>
2025-07-15 16:42:20 +0000

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,17 +1,18 @@
-export DATA_PATH="/home/easy_daily_articles/Poker_player/data/"
+export DATA_PATH="/home/alexlutt/Poker_player/data/"
 
+mkdir -p ${DATA_PATH}
 pip install -r requirements.txt
 
 huggingface-cli download RZ412/PokerBench --repo-type dataset --local-dir data/input/
 rm -rf data/input/.cache
 
-mkdir data/models/Qwen2-0.5B-Instruct
+mkdir -p data/models/Qwen2-0.5B-Instruct
 huggingface-cli download Qwen/Qwen2-0.5B-Instruct --local-dir data/models/Qwen2-0.5B-Instruct
 rm -rf data/models/Qwen2-0.5B-Instruct/.cache
 
-mkdir data/models/SmolLM2-135M-Instruct
+mkdir -p data/models/SmolLM2-135M-Instruct
 huggingface-cli download HuggingFaceTB/SmolLM2-135M-Instruct --local-dir data/models/SmolLM2-135M-Instruct
 rm -rf data/models/SmolLM2-135M-Instruct/.cache
 
-mkdir logs/
+mkdir -p logs/
 python3 -m source.clean_data > logs/data_cleaning.log 2>&1 &
